591035afffc3d4480dc59cb9771809b9cfe2de59
Michail Sygkelakis <mikesglks@gmail.com>
2020-04-22 12:02:25 +0300

    NPM Scripts part 1

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+$("#logiButtonid").click(function() {
+    $("#loginModal").modal("show");
+});
+$("#reserveTableButtonid").click(function() {
+    $("#reserveModal").modal("show");
+    $("#smokingLabel").addClass("active focus");
+});
+$("#smokingLabelid").click(function() {
+    $("#smokingLabel").removeClass("active focus");
+});
+$(document).ready(function() {
+    $("#mycarousel").carousel({
+        interval: 2000,
+    });
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else if (
+            $("#carouselButton").children("span").hasClass("fa-play")
+        ) {
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+});
